12b3fc4675ff57f36c707cd4d69b73165e7f89d1
GabriellBP <gbp@ic.ufal.br>
2018-09-26 17:24:59 -0300

    Alteração no método GET discussion, adicionado sistema de paginação, sendo /discussion/<int:discussion_id> o link default com page = 1 e /discussion/<int:discussion_id>/page/<int:page> para acessar diferentes páginas

--- API/webservice.py
+++ API/webservice.py
@@ -17,6 +17,9 @@ app.config['MYSQL_DB'] = database['db']
 CORS(app)
 mysql = MySQL(app)
 
+# Quantas instâncias por query, quando utilizado paginação
+PER_PAGE = 8
+
 
 @app.route("/")
 def index():
@@ -28,7 +31,8 @@ def index():
 def user(user_id):
     try:
         cur = mysql.connection.cursor()
-        cur.execute("SELECT u.firstname, u.lastname FROM mdl_user u WHERE u.id = %s" % str(user_id))
+        sql = "SELECT u.firstname, u.lastname FROM mdl_user u WHERE u.id = %s"
+        cur.execute(sql, [user_id])
         row_headers = [x[0] for x in cur.description]
         rv = cur.fetchone()
         json_data = dict(zip(row_headers, rv))
@@ -42,13 +46,13 @@ def user(user_id):
 def courses(user_id):
     try:
         cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT c.id, c.fullname, c.shortname FROM mdl_user u,  mdl_role_assignments ra,  mdl_context con, mdl_course c, mdl_role r WHERE u.id = ra.userid AND ra.contextid = con.id AND con.contextlevel = 50 AND con.instanceid = c.id AND ra.roleid = r.id AND r.shortname = 'student' AND u.id = " + str(
-                user_id))
+        sql = "SELECT c.id, c.fullname, c.shortname FROM mdl_user u,  mdl_role_assignments ra,  mdl_context con, mdl_course c, mdl_role r WHERE u.id = ra.userid AND ra.contextid = con.id AND con.contextlevel = 50 AND con.instanceid = c.id AND ra.roleid = r.id AND r.shortname = 'student' AND u.id = %s"
+        cur.execute(sql, [user_id])
         rv = cur.fetchall()
         json_data = []
         for result in rv:
-            cur.execute("SELECT `id`, `name`, `intro`  FROM mdl_forum WHERE course = " + str(result[0]))
+            sql = "SELECT `id`, `name`, `intro`  FROM mdl_forum WHERE course = %s"
+            cur.execute(sql, [result[0]])
             rvv = cur.fetchall()
             forums = []
             for f in rvv:
@@ -66,12 +70,11 @@ def courses(user_id):
 def forum(forum_id):
     try:
         cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT `id`, `type`, `name`, `intro`, `introformat`, `maxattachments`  FROM mdl_forum WHERE id = " + str(
-                forum_id))
+        sql = "SELECT `id`, `type`, `name`, `intro`, `introformat`, `maxattachments`  FROM mdl_forum WHERE id = %s"
+        cur.execute(sql, [forum_id])
         forum = cur.fetchone()
-        cur.execute("SELECT `id`, `name`, `userid`, `timemodified` FROM mdl_forum_discussions WHERE forum = " + str(
-            forum_id))
+        sql = "SELECT `id`, `name`, `userid`, `timemodified` FROM mdl_forum_discussions WHERE forum = %s"
+        cur.execute(sql, [forum_id])
         rv = cur.fetchall()
         discussions = []
         for result in rv:
@@ -87,24 +90,24 @@ def forum(forum_id):
 
 
 # GET /discussion/:discussion_id
-@app.route('/discussion/<int:discussion_id>')
-def discussion(discussion_id):
+@app.route('/discussion/<int:discussion_id>', defaults={'page': 1})
+@app.route('/discussion/<int:discussion_id>/page/<int:page>')
+def discussion(discussion_id, page):
     try:
+        start_at = (page - 1) * PER_PAGE
         cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT `id`, `name`, `firstpost`, `userid` FROM mdl_forum_discussions WHERE id = " + str(discussion_id))
+        sql = "SELECT `id`, `name`, `firstpost`, `userid` FROM mdl_forum_discussions WHERE id = %s"
+        cur.execute(sql, [discussion_id])
         discussion = cur.fetchone()  # informações abaixo + id_discussão + id_forum
-        cur.execute(
-            "SELECT p.id, p.parent, p.userid, p.modified, p.subject, p.message FROM mdl_forum_posts p WHERE p.discussion = " + str(
-                discussion_id) + " AND p.id = " + str(discussion[2]))
+        sql = "SELECT p.id, p.parent, p.userid, p.modified, p.subject, p.message FROM mdl_forum_posts p WHERE p.discussion = %s AND p.id = %s"
+        cur.execute(sql, [discussion_id, discussion[2]])
         first_post = cur.fetchone()  # informações firstpost
         username = json.loads(user(first_post[2]).get_data(as_text=True))
         first_post = {'id': first_post[0], 'parent': first_post[1], 'userid': first_post[2], 'username': username, 'modified': datetime.fromtimestamp(
             int(first_post[3])
         ).strftime('%d-%m-%Y %H:%M:%S'), 'subject': first_post[4], 'message': first_post[5], 'qtt_answers': 0}
-        cur.execute(
-            "SELECT p.id, p.parent, p.userid, p.modified, p.subject, p.message FROM mdl_forum_posts p WHERE p.discussion = " + str(
-                discussion_id) + " AND p.parent = " + str(discussion[2]) + " ORDER BY p.id DESC")
+        sql = "SELECT p.id, p.parent, p.userid, p.modified, p.subject, p.message FROM mdl_forum_posts p WHERE p.discussion = %s AND p.parent = %s ORDER BY p.id DESC LIMIT %s, %s"
+        cur.execute(sql, [discussion_id, discussion[2], start_at, PER_PAGE])
         # row_headers = [x[0] for x in cur.description]
         rv = cur.fetchall()
         posts = []
@@ -132,8 +135,8 @@ def answer(answer_id):
         return jsonify({'status': 404, 'mensagem': 'id inválido'})
     try:
         cur = mysql.connection.cursor()
-        cur.execute("SELECT p.id, p.userid, p.modified, p.subject, p.message, p.discussion FROM mdl_forum_posts p WHERE p.parent = " + str(
-            answer_id) + " ORDER BY p.id DESC")
+        sql = "SELECT p.id, p.userid, p.modified, p.subject, p.message, p.discussion FROM mdl_forum_posts p WHERE p.parent = %s ORDER BY p.id DESC"
+        cur.execute(sql, [answer_id])
         rv = cur.fetchall()
         json_data = []
         for result in rv:
@@ -157,13 +160,13 @@ def new_discussion(forum_id):
         data = request.json
         timenow = datetime.timestamp(datetime.now())
         cur = mysql.connection.cursor()
-        cur.execute(sql, (data['course'], forum_id, data['title'], data['userid'], int(timenow)))
+        cur.execute(sql, [data['course'], forum_id, data['title'], data['userid'], int(timenow)])
         cur.fetchall()
         discussion = get_last_record("mdl_forum_discussions")
         new_post(discussion['id'], data['message'], data['title'], data['userid'])
         post = get_last_record("mdl_forum_posts")
         sql = "UPDATE mdl_forum_discussions SET `firstpost` = %s WHERE `id` = %s"
-        cur.execute(sql, (post['id'], discussion['id']))
+        cur.execute(sql, [post['id'], discussion['id']])
         mysql.connection.commit()
         return jsonify({'status': 200, 'mensagem': 'Discussão salva com sucesso!'})
     except Exception as e:
@@ -180,13 +183,13 @@ def new_post(discussion_id, message="", subject="", userid=-1):
         timenow = datetime.timestamp(datetime.now())
         cur = mysql.connection.cursor()
         if userid != -1:
-            cur.execute(sql, (discussion_id, 0, userid, int(timenow), int(timenow), subject, message))
+            cur.execute(sql, [discussion_id, 0, userid, int(timenow), int(timenow), subject, message])
         else:
-            cur.execute(sql, (
+            cur.execute(sql, [
             discussion_id, data['postParent'], data['userid'], int(timenow), int(timenow), data['subject'],
-            data['message']))
+            data['message']])
             sql = "UPDATE mdl_forum_discussions SET `timemodified` = %s, `usermodified` = %s WHERE `id` = %s"
-            cur.execute(sql, (int(timenow), data['userid'], discussion_id))
+            cur.execute(sql, [int(timenow), data['userid'], discussion_id])
             mysql.connection.commit()
         return jsonify({'status': 200, 'mensagem': 'Post salvo com sucesso!'})
     except Exception as e:
@@ -210,12 +213,12 @@ def get_last_record(table):
 def get_qtt_answers(post_id, discussion_id):
     try:
         with mysql.connection.cursor() as cursor:
-            sql = "SELECT COUNT(id) as 'count' FROM mdl_forum_posts WHERE parent = %s AND  discussion = %s"
-            cursor.execute(sql, (str(post_id), str(discussion_id)))
+            sql = "SELECT COUNT(id) as 'count' FROM mdl_forum_posts WHERE parent = %s AND discussion = %s"
+            cursor.execute(sql, [str(post_id), str(discussion_id)])
             row_headers = [x[0] for x in cursor.description]
-            qtd = cursor.fetchone()
-            qtd = dict(zip(row_headers, qtd))
-            return qtd['count']
+            qtt = cursor.fetchone()
+            qtt = dict(zip(row_headers, qtt))
+            return qtt['count']
     except Exception as e:
         print(e)
         return jsonify({'status': 500, 'mensagem': 'Internal Server Error'})
